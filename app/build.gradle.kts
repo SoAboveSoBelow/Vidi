95c602c1ab056740c3a6d9ad48e065e7b95663e1
SoAboveSoBelow <Kingsroot1892@proton.me>
2026-09-18 20:47:59 -0500

    Bump version to 0.19.13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,8 +43,8 @@ android {
     defaultConfig {
         applicationId = "xyz.Quickdev.Vidi.mi"
 
-        versionCode = 152
-        versionName = "0.19.12"
+        versionCode = 153
+        versionName = "0.19.13"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getLatestCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getLatestCommitSha()}\"")
